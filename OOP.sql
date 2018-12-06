b7b7bc016e2fb52474b9a5854969d6e1bbb091ee
banditek85 <mitjasusec@outlook.com>
2018-12-06 09:46:09 +0100

    Creating types

--- OOP.sql
+++ OOP.sql
@@ -17,3 +17,50 @@ CREATE OR REPLACE TYPE BODY Animal IS
       RETURN name || '  says woof.';
     END;
 END;
+-- ********************************************* --
+
+CREATE OR REPLACE TYPE Rectangle IS OBJECT 
+(
+  length NUMBER,
+  height NUMBER,
+  MEMBER PROCEDURE display,
+  -- Order functions are called when comparison operators are called automatically between two instances of this type. Order functions receive another instance as a parameter and the order is defined by internal logic of the function. Map functions are the same, except that order is defined by internal type attributes and they don't except parameters. Type can only contain one order OR map function, but not both.
+  ORDER MEMBER FUNCTION compare(r Rectangle) RETURN NUMBER
+);
+/
+
+CREATE OR REPLACE TYPE BODY Rectangle IS 
+  MEMBER PROCEDURE display IS
+  BEGIN
+  dbms_output.put_line(TO_CHAR(self.length + self.height));
+  END display;
+  ORDER MEMBER FUNCTION compare(r Rectangle) RETURN NUMBER IS
+  BEGIN
+    IF self.length * self.height > r.length * r.height THEN
+      RETURN 1;
+    ELSE 
+      RETURN -1;
+    END IF;
+  END compare;
+END;
+
+
+-- Example program
+DECLARE
+  box1 Rectangle := Rectangle(1, 1);
+  box2 Rectangle := Rectangle(3,3);
+  compare_result number;
+BEGIN
+  -- Calling member procedures, parenthesis are optional.
+  box1.display();
+  box2.display();
+  
+  -- Calling order function
+  compare_result := box1.compare(box2);
+  dbms_output.put_line(compare_result);
+
+  IF box1 > box2 THEN 
+  dbms_output.put_line('First bigger');
+  ELSE dbms_output.put_line('Second bigger');
+  END IF;
+END;
