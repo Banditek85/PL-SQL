f0481b41fca43fce216869d428f09c889795b749
banditek85 <mitjasusec@outlook.com>
2018-12-07 20:38:24 +0100

    updated OOP.sql

--- OOP.sql
+++ OOP.sql
@@ -2,7 +2,7 @@
 
 -- Creating object types as user defined data types, similar to classes in other programming languages.
 
--- You create objects on data-base level to be able to instantiate instances of them in pl/sql blocks, subprograms or packages. Declaration part is public part
+-- You create objects on data-base level to be able to create instances of them in pl/sql blocks, subprograms or packages. Declaration part is public part
 CREATE OR REPLACE TYPE Animal IS OBJECT 
 (
   name varchar2(100),
@@ -24,7 +24,7 @@ CREATE OR REPLACE TYPE Rectangle IS OBJECT
   length NUMBER,
   height NUMBER,
   MEMBER PROCEDURE display,
-  -- Order functions are called when comparison operators are called automatically between two instances of this type. Order functions receive another instance as a parameter and the order is defined by internal logic of the function. Map functions are the same, except that order is defined by internal type attributes and they don't except parameters. Type can only contain one order OR map function, but not both.
+  -- Order functions are called when comparison operators are called automatically between two instances of this type. Order functions receive another instance as a parameter and the order is defined by internal logic of the function. Map functions are the same, except that order is defined by internal type attribute and they don't except parameters. Type can only contain one order OR map function, but not both.
   ORDER MEMBER FUNCTION compare(r Rectangle) RETURN NUMBER
 );
 /
@@ -44,14 +44,13 @@ CREATE OR REPLACE TYPE BODY Rectangle IS
   END compare;
 END;
 
-
 -- Example program
 DECLARE
   box1 Rectangle := Rectangle(1, 1);
   box2 Rectangle := Rectangle(3,3);
   compare_result number;
 BEGIN
-  -- Calling member procedures, parenthesis are optional.
+  -- Calling member procedures and functions, parenthesis are optional.
   box1.display();
   box2.display();
   
