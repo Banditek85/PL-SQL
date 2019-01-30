5c88b0979c0342fd5f11e375fdd7e5d187caf75b
banditek85 <mitjasusec@outlook.com>
2019-01-30 15:13:51 +0100

    added bulk_collect

--- bulk_collect.sql
+++ bulk_collect.sql
@@ -1,3 +1,6 @@
+-- Bulk binding is used to reduce the amount of code and more importantly to reduce context switches between SQL and PL/SQL
+-- engines. Every time you have a non-SELECT DML statement inside any kind of loop, you should consider switching to bulk binding. 
+
 declare
     type string_table is table of varchar2(50);
     t_names string_table := new string_table();
@@ -13,10 +16,10 @@ begin
   end loop;
 */
 
-/* Shorter way of doing same thing as above is with bulk collect, which implicitly extends collection
+/* Shorter way of doing same thing as above is with bulk collect (here used with implicit cursor), which implicitly extends collection
 and inserts the value:
 */
-select  first_name bulk collect into t_names from employees;
+select first_name bulk collect into t_names from employees;
 
 -- Printing the nested table:
     for i in t_names.first..t_names.last
@@ -24,3 +27,70 @@ select  first_name bulk collect into t_names from employees;
     dbms_output.put_line(t_names(i));
     end loop;
 end;
+
+/* PL/SQL collections are held in memory, which on large data sets can represent performance problem. For this reason LIMIT clause
+is provided which is used to fetch and process smaller chunks of data in phases */
+
+DECLARE
+-- Declaring a collection type and collection type variable to temporarily store the data
+  TYPE t_buffer IS TABLE OF countries.country_name%TYPE;
+  v_buffer t_buffer := new t_buffer();
+  
+-- Declaring the cursor  
+  CURSOR c_data IS 
+                SELECT country_name FROM countries;
+                
+BEGIN
+  OPEN c_data;
+    LOOP
+      FETCH c_data BULK COLLECT INTO v_buffer
+      LIMIT 5;
+      -- When cursor did not fetch anything, exit the loop
+      EXIT WHEN v_buffer.COUNT = 0;
+      
+      -- Process the data here
+        FOR i IN 1..v_buffer.COUNT
+          LOOP
+            dbms_output.put_line(v_buffer(i)); 
+          END LOOP;
+    END LOOP;
+  CLOSE c_data;
+END;
+/
+
+-- Without using the LIMIT clause all of the data can be fetched at once and is available in the collection after the fetch
+-- completes
+
+DECLARE
+  TYPE t_buffer IS TABLE OF countries.country_name%TYPE;
+  v_buffer t_buffer := new t_buffer();
+  
+  CURSOR c_data IS 
+                SELECT country_name FROM countries;
+
+BEGIN
+  OPEN c_data;
+    FETCH c_data BULK COLLECT INTO v_buffer;
+  CLOSE c_data;
+END;
+
+
+-- Or fetch just first 10 rows in the table
+/*
+BEGIN
+   OPEN c_data;
+     FETCH c_data BULK COLLECT INTO v_buffer
+     LIMIT 10;
+   CLOSE c_data;
+ END;
+*/ 
+
+
+
+
+
+
+
+
+
+
--- pseudocolumns.sql
+++ pseudocolumns.sql
@@ -1,12 +1,14 @@
 /* 
-Pseudo-columns are Oracle assigned values (pseudo-fields) used in the same context as database columns, but are not insertable or stored in the database. 
+Pseudo-columns are Oracle assigned values (pseudo-fields) used in the same context as database columns, but are not insertable
+or stored in the database. 
 Examples: ROWNUM, SYSDATE, SYSTIMESTAMP, ROWID, CURRVAL, NEXTVAL etc.
 */
 
 SELECT SYSDATE, SYSTIMESTAMP FROM dual;
 
 /*
-ROWNUM pseudo-column returns a number indicating the order in which a row was selected from a table so it can be used to limit the result set of our query.
+ROWNUM pseudo-column returns a number indicating the order in which a row was selected from a table so it can be used to limit
+the result set of our query.
 */ 
 
 -- This selects only first three rows of type 'Cruiser':
@@ -14,13 +16,15 @@ SELECT ship_id, ship_name, port_num from ships
 WHERE ship_type = 'Cruiser' AND ROWNUM <= 3;
 
 /*
-ROWNUM is incremented only when the previous row has it's own value assigned. That's why this will not return any rows, because ROWNUM is not greater than 1 for the first row and condition is always false.
+ROWNUM is incremented only when the previous row has it's own value assigned. That's why this will not return any rows, because
+ROWNUM is not greater than 1 for the first row and condition is always false.
 */
 SELECT * FROM employees
 WHERE ROWNUM > 1;
 
 /*
-ROWNUM pseudo-column has it's value assigned BEFORE any ordering and grouping of the result set is done, so we need to do these operations (if they are needed) in a separated subquery. This will select first top 5 paid employees.
+ROWNUM pseudo-column has it's value assigned BEFORE any ordering and grouping of the result set is done, so we need to do these
+operations (if they are needed) in a separated subquery. This will select first top 5 paid employees.
 */
 SELECT * FROM 
   (SELECT * FROM employees 
